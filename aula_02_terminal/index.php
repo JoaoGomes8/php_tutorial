d4668a58813861397c55c1d9608eac2c2e82e6b0
joao <joaogomes2609@gmail.com>
2026-02-21 14:29:00 +0000

    aula 2 e 3 - terminal e comentários

---
+++ aula_02_terminal/index.php
@@ -0,0 +1,3 @@
+<?php
+echo "Teste de terminal" . PHP_EOL;
+print "Novo teste";
---
+++ aula_03_comentarios/index_2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Comentário de HTML. Vai aparecer no código fonte -->
+
+    <?php // Comentário de PHP. Não aparece no código fonte ?>
+
+    <h1>Teste de comentários</h1>
+</body>
+</html>
