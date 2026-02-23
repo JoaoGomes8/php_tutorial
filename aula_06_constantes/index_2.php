706559171720893a643347b27201026a6b6b6e35
Joao <joaogomes2609@gmail.com>
2026-02-23 11:00:33 +0000

    aula 6 - constantes

---
+++ aula_06_constantes/index_2.php
@@ -0,0 +1,12 @@
+<?php
+
+    // também podemos definir constantes com a expressão const
+
+    const NOME = "João";
+    echo NOME;
+
+    /*
+    Esta forma de definição de constatntes não é muito utilizada.
+    Com define, é possivel definir constantes em qualquer parte do código.
+    Com const, existem estruturas dentro das quais não podemos criar as constantes.
+    */
---
+++ aula_06_constantes/index_3.php
@@ -0,0 +1,13 @@
+<?php
+
+    // o PHP tem as suas próprias constantes.
+
+    echo PHP_VERSION;
+    echo "<br>";
+
+    // e tem um tipo de constantes que são definidas de forma dinâmica.
+    // são designadas por constantes mágicas.
+
+    echo "Estou a executar este código na linha " . __LINE__ ;
+    echo "<br>";
+    echo "Estou a executar este código na linha " . __LINE__ ;
