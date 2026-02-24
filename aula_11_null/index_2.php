ec3f106f80b1e32465d87a0d5002bb3cc17b55e2
Joao <joaogomes2609@gmail.com>
2026-02-24 14:11:45 +0000

    aula 11 - null

---
+++ aula_11_null/index_2.php
@@ -0,0 +1,14 @@
+<?php
+
+    // NULL
+
+    $valor = null;
+    if (is_null($valor)) {
+        echo 'É null';
+    }
+
+    echo '<br>';
+
+    if (empty($valor)) {
+        echo 'É nulo ou vazio';
+    }
